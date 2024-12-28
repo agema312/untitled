4a20ffda83ed6cc472776911dba3cdc03c1fe96f
agema312 <142101432+agema312@users.noreply.github.com>
2024-12-28 20:47:01 +0600

    Update Glovo.java

--- src/main/java/Glovo/Glovo.java
+++ src/main/java/Glovo/Glovo.java
@@ -4,5 +4,7 @@ public class Glovo {
     public static void main(String[] args) {
         // Glovo comments
         System.out.println("Glovo");
+        System.out.println("Glovo2");
+        System.out.println("Glovo3");
     }
 }
